2081043ae001172c369cbb059d5ef8bd8dcead48
chukins <153525841+chukins@users.noreply.github.com>
2024-09-17 11:15:03 +0100

    gitignore

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 from flask import Flask, request, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
