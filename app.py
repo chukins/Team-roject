cd7ebc9ec1104f8606f66c93d49d90ab1fb76e39
Matthew Martin <334606@edu.mkcollege.ac.uk>
2024-09-17 15:32:40 +0100

    made log in page work

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, request, render_template
 import sqlite3
 
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute('''
+            INSERT INTO LoginDetails (Username, Password)
+            VALUES (?, ?)
+            ''', (username, password))
+        conn.commit()
+        conn.close()
+    return render_template('login.html')
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
