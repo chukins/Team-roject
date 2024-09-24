c0ac729774680de62640714c5080f2a9de4c4a7b
Matthew Martin <334606@edu.mkcollege.ac.uk>
2024-09-24 12:17:12 +0100

    fixed a lot of things and made the bmi page accessible

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import sqlite3
 import hashlib
+import string
+acceptedChars = [x for x in string.punctuation+string.ascii_letters+string.digits]
 
 app = Flask(__name__)
 
@@ -12,15 +14,27 @@ def index():
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
+        if len(username) <= 5 or len(username) >= 13:
+            return render_template('error.html', error='Username must be between 6 and 12 characters')
+        for letter in username:
+            if letter not in acceptedChars:
+                return render_template('error.html', error='Username contains invalid characters')
+            
         password = request.form.get('password')
-        hashed = hashlib.md5(password.encode())
-        print(hashed.hexdigest())
+        if len(password) <= 7:
+            return render_template('error.html', error='Password must be at least 8 characters, including a number and a special character')
+        for letter in password:
+            if letter not in acceptedChars:
+                return render_template('error.html', error='Password contains invalid characters')
+            
+        hashed = hashlib.sha512(password.encode()).hexdigest()
+        print(hashed)
         conn = sqlite3.connect('database.db')
         c = conn.cursor()
         c.execute('''
             INSERT INTO LoginDetails (Username, Password)
             VALUES (?, ?)
-            ''', (username, hashed.hexdigest()))
+            ''', (username, hashed))
         conn.commit()
         conn.close()
     return render_template('login.html')
@@ -34,6 +48,10 @@ def workplan():
 @app.route('/about', methods=['POST', 'GET'])
 def about():
     return render_template('about.html')
+
+@app.route('/bmi', methods=['POST', 'GET'])
+def bmi():
+    return render_template('bmi.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
